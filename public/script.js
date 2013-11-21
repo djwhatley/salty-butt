f56b1d8a9f1e0acd90e5bf2e02317196126507ba
Anthony Taormina <anthony.m.taormina@gmail.com>
2013-11-21 17:18:43 -0500

    Spinner logic implemented

--- public/script.js
+++ public/script.js
@@ -1,4 +1,14 @@
+var blueButton = document.getElementById("betbuttonblue");
+var redButton = document.getElementById("betbuttonred");
+var selectedPlayer = document.getElementById("selectedplayer");
+blueButton.onclick = function () {
+    selectedPlayer.value = "1";
+};
+redButton.onclick = function () {
+    selectedPlayer.value = "0";
+};
+
 var es = new EventSource('/refresh');
-es.onmessage = function(e) {
+es.onmessage = function() {
 	window.location.reload(true);  
-}
+};
